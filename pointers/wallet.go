6f418003c00833428e5b54e265f644340118a01d
JoaoHFerreira <jaycops@gmail.com>
2023-05-22 17:36:48 -0300

    adjust

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,13 +1,42 @@
-package pointers
+package main
 
-type Wallet struct {
-	balance int
+import (
+	"errors"
+	"fmt"
+)
+
+// Bitcoin represents a number of Bitcoins.
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
-func (w Wallet) Deposit(amount int) {
+// Wallet stores the number of Bitcoin someone owns.
+type Wallet struct {
+	balance Bitcoin
+}
+
+// Deposit will add some Bitcoin to a wallet.
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+// ErrInsufficientFunds means a wallet does not have enough Bitcoin to perform a withdraw.
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+// Withdraw subtracts some Bitcoin from the wallet, returning an error if it cannot be performed.
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+// Balance returns the number of Bitcoin a wallet has.
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
