121fdc2ed3ce18f229083f4a2eca8e2547ef1371
JoaoHFerreira <jaycops@gmail.com>
2023-05-31 17:37:32 -0300

    create struct for terminal

--- go_terminal/go_terminal.go
+++ go_terminal/go_terminal.go
@@ -9,21 +9,53 @@ import (
 
 const ONE_SECOND time.Duration = 1 * time.Second
 
-func getTerminalCMD() string {
+type terminal struct {
+	pwd  string
+	dirs map[string][]string
+}
+
+func (t *terminal) run() {
+	for {
+		cmd := t.getCMD()
+
+		switch cmd {
+		case "pwd\n":
+			t.current_path()
+		case "ls\n":
+			fmt.Println("this is a ls")
+		case "touch\n":
+			fmt.Println("this is a touch")
+		case "echo\n":
+			fmt.Println("this is echo")
+		case "cd\n":
+			fmt.Println("this is cd")
+		case "mkdir\n":
+			fmt.Println("this is echo")
+		default:
+			fmt.Printf("%s: command not found\n", cmd[:len(cmd)-1])
+		}
+
+		fmt.Println("This is my terminal: ", *t)
+	}
+
+}
+
+func (t *terminal) getCMD() string {
+	fmt.Printf("/")
 	keyWord, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 	return keyWord
 }
 
+func (t *terminal) current_path() {
+	fmt.Println(t.pwd)
+}
+
 func main() {
-	terminalMap := map[string]string{
-		"pwd": "/",
+	goTerminal := terminal{
+		pwd: "/",
+		dirs: map[string][]string{
+			"/": {"/"},
+		},
 	}
-
-	for {
-
-		cmd := getTerminalCMD()
-		time.Sleep(ONE_SECOND)
-		fmt.Println("This is my terminal: ", cmd)
-		fmt.Println("This is my terminal: ", terminalMap)
-	}
+	goTerminal.run()
 }
