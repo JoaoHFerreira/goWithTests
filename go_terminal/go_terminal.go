a7fc83f4771e385432ab1a28ee503ff88e5a929b
JoaoHFerreira <jaycops@gmail.com>
2023-06-01 17:20:23 -0300

    finish rough logic

--- go_terminal/go_terminal.go
+++ go_terminal/go_terminal.go
@@ -42,7 +42,7 @@ func (t *terminal) run() {
 }
 
 func (t *terminal) getCMD() string {
-	fmt.Printf("/")
+	fmt.Printf(t.pwd)
 	keyWord, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 	return keyWord
 }
@@ -52,28 +52,47 @@ func (t *terminal) currentPath() {
 }
 
 func (t *terminal) listCurrentDir() {
-	totalFiles := append(t.dirs[t.pwd], t.files[t.pwd]...)
+	var totalFiles []string
+	totalFiles = append(totalFiles, t.dirs[t.pwd]...)
+	totalFiles = append(totalFiles, t.files[t.pwd]...)
 	fmt.Println(totalFiles)
 }
 
 func (t *terminal) touch(raw_cmd string) {
-	filesOnly := strings.Split(raw_cmd, " ")[1:]
+	filesOnly := strings.Split(strings.TrimSpace(raw_cmd), " ")[1:]
 	t.files[t.pwd] = append(t.files[t.pwd], filesOnly...)
 }
 
 func (t *terminal) echo(raw_cmd string) {
-	fmt.Println("here, only the echo key word will be excluded")
+	echoPart := strings.Split(raw_cmd, " ")[1:]
+	joinedString := strings.Join(echoPart, " ")
+	fmt.Println(joinedString)
 }
 
 func (t *terminal) cd(raw_cmd string) {
-	splitedCommands := strings.Split(raw_cmd, " ")
-	dirToGo := splitedCommands[len(splitedCommands)-1]
-	fmt.Println(t.dirs[dirToGo])
+	desiredPath := strings.TrimSpace(strings.Split(raw_cmd, " ")[1])
+
+	if desiredPath == "/" {
+		t.pwd = desiredPath
+		return
+	}
+
+	_, exists := t.dirs[t.pwd+desiredPath+"/"]
+
+	if exists {
+		t.pwd = t.pwd + desiredPath + "/"
+	} else {
+		fmt.Println("cd: no such file or directory:", desiredPath)
+	}
 }
 
 func (t *terminal) mkdir(raw_cmd string) {
-	dirsOnly := strings.Split(raw_cmd, " ")[1:]
-	t.dirs[t.pwd] = append(t.dirs[t.pwd], dirsOnly...)
+	dirsOnly := strings.Split(strings.TrimSpace(raw_cmd), " ")[1:]
+	for _, path := range dirsOnly {
+		composeString := t.pwd + path + "/"
+		t.dirs[t.pwd] = append(t.dirs[t.pwd], composeString)
+		t.dirs[composeString] = []string{} // Initialize the dirs slice for each new directory
+	}
 }
 
 func main() {
